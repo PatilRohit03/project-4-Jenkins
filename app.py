d0ced8780c670dd4ff562536c00c8487729c082f
Rohit <rohitkumarpatil04@gmail.com>
2024-10-23 09:51:02 -0400

    updated files

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, Jenkins!"
+    return 'Hello, Jenkins!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', host='0.0.0.0')
+
